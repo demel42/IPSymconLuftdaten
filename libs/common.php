50e34de6b9d4e4a6e23b2625ac0a2ba499e75bf9
demel42 <demel42@web.de>
2018-05-29 16:25:50 +0200

    LogMessage() ist protected

--- libs/common.php
+++ libs/common.php
@@ -92,7 +92,7 @@ trait LuftdatenCommon
         return 'text/plain';
     }
 
-    private function LogMessage($Message, $Severity)
+    protected function LogMessage($Message, $Severity)
     {
         if (IPS_GetKernelVersion() >= 5) {
             switch ($Severity) {
