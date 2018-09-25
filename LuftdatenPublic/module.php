ecd9f3f20389cefdb1fd07968563f02ee26621a7
demel42 <demel42@web.de>
2018-09-25 17:10:08 +0200

    round() statt floor() verwenden

--- LuftdatenPublic/module.php
+++ LuftdatenPublic/module.php
@@ -145,7 +145,7 @@ class LuftdatenPublic extends IPSModule
         $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         curl_close($ch);
 
-        $duration = floor((microtime(true) - $time_start) * 100) / 100;
+        $duration = round(microtime(true) - $time_start, 2);
         $this->SendDebug(__FUNCTION__, ' => httpcode=' . $httpcode . ', duration=' . $duration . 's', 0);
 
         $statuscode = 0;
